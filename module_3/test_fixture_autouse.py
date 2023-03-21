25fe25069dd548d65f3cffc580062171f914710b
shev7a <sheva7rus@gmail.com>
2023-03-21 12:26:41 +0300

    add examples of autouse and scope in fixtures

---
+++ module_3/test_fixture_autouse.py
@@ -0,0 +1,36 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+@pytest.fixture(scope="class")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
+@pytest.fixture(autouse=True)
+def prepare_data():
+    print()
+    print("preparing some critical data for every test")
+
+
+class TestMainPage1:
+
+    def test_guest_should_see_login_link(self, browser):
+        print("start test1")
+        # не передаём как параметр фикстуру prepare_data, но она все равно выполняется
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "#login_link")
+        print("finish test1")
+
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        print("start test2")
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+        print("finish test2")
