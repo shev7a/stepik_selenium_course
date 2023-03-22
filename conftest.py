bd0c086b962c116ad3987a0dd36299279ed30db1
shev7a <sheva7rus@gmail.com>
2023-03-22 14:06:36 +0300

    add choosing browser in command line

--- conftest.py
+++ conftest.py
@@ -1,11 +1,23 @@
 import pytest
 from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
 
 
 @pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
     yield browser
     print("\nquit browser..")
     browser.quit()
